306b6c47baff4f91999a8b4dffa8ea52c3baf798
Alexandros Polymeros <alpolymer@gmail.com>
2023-10-01 03:07:29 +0300

    game array and random pairs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react"
-const App = () => {
-  const [rows, setRows] = useState(0)
-  const [cols, setCols] = useState(0)
 
-  const createArray = () => {
+function generatePairs(numPairs, minRange, maxRange) {
+  const pairs = []
+
+  for (let i = 0; i < numPairs; i++) {
+    const num = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange
+    pairs.push([num, num])
+  }
+
+  return pairs
+}
+
+// Example usage: generate 3 pairs of equal integers within the range 1 to 10
+const result = generatePairs(3, 1, 10)
+console.log("RANDOM PAIRS:", result)
+
+const App = () => {
+  const [rows, setRows] = useState(3)
+  const [cols, setCols] = useState(3)
+
+  function createArray() {
     let arr = []
     for (let i = 0; i < rows; i++) {
       arr[i] = []
@@ -15,8 +31,20 @@ const App = () => {
     return arr
   }
 
-  const arr = createArray(5, 3)
-  console.log(arr)
+  function gameArray() {
+    let arr = createArray()
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        arr[i][j] = Math.floor(Math.random() * 8)
+      }
+    }
+    return arr
+  }
+
+  const arrNull = createArray()
+  const arrGame = gameArray()
+  console.log(arrGame)
+
   return (
     <main className="h-screen bg-slate-500">
       {cols === 0 && rows === 0 ? (
@@ -24,7 +52,7 @@ const App = () => {
           Set the number of cards to play!
         </div>
       ) : (
-        arr.map((row, ridx) => {
+        arrNull.map((row, ridx) => {
           return (
             <div key={ridx} className="flex justify-center my-auto">
               {row.map((cell, cidx) => {
