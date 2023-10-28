bed741393266126305d984af0739cf7b421f427c
Alexandros Polymeros <alpolymer@gmail.com>
2023-10-28 19:24:42 +0300

    conditional start

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -42,45 +42,80 @@ export default function InitialScreen({
         ))}
       </div>
       <div className="flex flex-col justify-center items-center mt-10">
-        <p className="font-pixel text-3xl text-orange-500">
-          {" "}
-          Please type your names you mem freaks!!!
-        </p>
+        {!selectedNumber ? (
+          <p className="font-pixel text-3xl text-orange-500 blur-sm">
+            {" "}
+            Please type your names you mem freaks!!!
+          </p>
+        ) : (
+          <p className="font-pixel text-3xl text-orange-500">
+            {" "}
+            Please type your names you mem freaks!!!
+          </p>
+        )}
         <form className="flex flex-col justify-center items-center ">
-          <div className="flex justify-center gap-44 mt-2">
-            <div className="flex flex-col items-center">
-              <label className="font-pixel text-xl text-orange-500">
-                Player 1
-              </label>
-              <input
-                type="text"
-                value={player1}
-                className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
-                onChange={(e) => onSetPlayer1(e.target.value)}
-              />
-            </div>
+          {!selectedNumber ? (
+            <div className="flex justify-center gap-44 mt-2 blur-sm">
+              <div className="flex flex-col items-center">
+                <label className="font-pixel text-xl text-orange-500">
+                  Player 1
+                </label>
+                <input
+                  type="text"
+                  value={player1}
+                  className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
+                  onChange={(e) => onSetPlayer1(e.target.value)}
+                />
+              </div>
 
-            <div className="flex flex-col items-center">
-              <label className="font-pixel text-xl text-orange-500">
-                Player 2
-              </label>
-              <input
-                type="text"
-                value={player2}
-                className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
-                onChange={(e) => onSetPlayer2(e.target.value)}
-              />
+              <div className="flex flex-col items-center">
+                <label className="font-pixel text-xl text-orange-500">
+                  Player 2
+                </label>
+                <input
+                  type="text"
+                  value={player2}
+                  className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
+                  onChange={(e) => onSetPlayer2(e.target.value)}
+                />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="flex justify-center gap-44 mt-2">
+              <div className="flex flex-col items-center">
+                <label className="font-pixel text-xl text-orange-500">
+                  Player 1
+                </label>
+                <input
+                  type="text"
+                  value={player1}
+                  className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
+                  onChange={(e) => onSetPlayer1(e.target.value)}
+                />
+              </div>
 
-          <button
-            className="mt-20 font-pixel text-6xl text-orange-500 font-bold"
-            onClick={
-              selectedNumber ? () => onStart(true) : () => onStart(false)
-            }
-          >
-            Play
-          </button>
+              <div className="flex flex-col items-center">
+                <label className="font-pixel text-xl text-orange-500">
+                  Player 2
+                </label>
+                <input
+                  type="text"
+                  value={player2}
+                  className="rounded-md h-10 p-3 text-center border-4 border-orange-300 focus:outline-none focus:border-orange-500 font-pixel"
+                  onChange={(e) => onSetPlayer2(e.target.value)}
+                />
+              </div>
+            </div>
+          )}
+
+          {player1 !== "" && player2 !== "" && selectedNumber && (
+            <button
+              className="mt-20 font-pixel text-6xl text-orange-500 font-bold"
+              onClick={() => onStart(true)}
+            >
+              Play
+            </button>
+          )}
         </form>
       </div>
     </>
